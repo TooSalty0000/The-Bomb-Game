b8c3945bd0a9be548fc24d9a2338637da779466d
JeffDaTurtle <57203585+JeffDaTurtle@users.noreply.github.com>
2022-02-08 16:36:36 +0800

    Big Button
    Worked on Big Button
    Added some functions to interactable

--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -2,10 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Interactable : MonoBehaviour
+public class Interactable : MonoBehaviour 
 {
-    public virtual void Interact()
-    {
+    /*
+        If there is a module where the user's hand can interact, (continue to operate the function). 
+
+        (continue to operate the function):
+        - Detect and store exactly which object the user's hand operates on.
+        - Call on the corresponding script of any given object to operate on it.
+    */
+    private float startTime = 0f;
+    private float endTime = 0f;
+    public virtual void Interact() {
         Debug.Log("Interacting with " + gameObject.name);
+
+        if (Input.GetMouseButtonDown(0))
+            startTime = Time.time;
+
+        if (Input.GetMouseButtonUp(0))
+            endTime = Time.time;
+        
+        if (endTime - startTime > 0.5) {
+            startTime = 0;
+            endTime = 0;
+        }
+    }
+
+    public virtual void InteractHold() {
+
     }
 }
