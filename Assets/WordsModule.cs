8d6e32c79e922d42533e96ca8dc3cec314c139a0
TooSalty <omegacastle10@gmail.com>
2022-03-15 16:38:26 +0800

    Words Module

--- Assets/WordsModule.cs
+++ Assets/WordsModule.cs
@@ -5,33 +5,61 @@ using TMPro;
 
 public class WordsModule : Module
 {
-    private int value = 1;
+    private int level = 1;
     [SerializeField]
     private TextMeshPro valueText;
+    private TextMeshPro problemText;
+    private string[] words = new string[] {
+        "BOMB", "CUT", "CHEM", "TEMP", "WIRE", "EXPLODE", "DROP", "COLOR"
+    };
+
+    private string currentWord;
+    private int answerValue;
+    
+    private int value = 0;
+    int randomIndex = 0;
     // Start is called before the first frame update
     void Start() {
+        setAnswerValue();
         
-    }
-
-    // Update is called once per frame
-    void Update() {
-        valueText.text = value.ToString();
-    }
-
-    public void pressed(string direction) {
-        if (direction == "Up") {
-            if (value < 9) {
-                value++;
-            }
-        } else if (direction == "Down") {
-            if (value > 1) {
-                value--;
-            }
-        }
     }
 
     public void submit() {
         
+        checkAnswer();
+    }
+
+    void setAnswerValue() {
+        switch(level){
+            case 1:
+                randomIndex = Random.Range(0, words.Length);
+                answerValue = randomIndex;
+                break;
+            case 2:
+                answerValue = words[randomIndex].Length - 3;
+                break;
+            case 3:
+                if(randomIndex == 2 || randomIndex == 7 || randomIndex == 3 || randomIndex == 4){
+                    answerValue = randomIndex;
+                }else if(randomIndex == 1 || randomIndex == 6 || randomIndex == 5){
+                    answerValue = 3;
+                }else if(randomIndex == 0){
+                    answerValue = 9;
+                }
+                break;
+        }
+    }
+
+    void checkAnswer() {
+        if (answerValue == value) {
+            if (level < 2) {
+                level++;
+            } else {
+                solved();
+            }
+        } else {
+            fail();
+        }
     }
 
 }
